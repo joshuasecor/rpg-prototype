909d5ad5e5f18f25f3b8ef3a69e8f2777a52c040
joshuasecor <joshuasecor@gmail.com>
2016-01-02 19:12:45 -0800

    damage and defense logic

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
 function attackSelect() {
   var charId = prompt("Enter the ID of the character you are attacking");
-  attackChar(charId);
-};
-
-function attackChar(charId) {
-	var charId = "char" + charId;
+  charId = "char" + charId;
 	damage(charId);
 };
 
 function damage(charId) {
-	characters[charId]["hp"] = (characters[charId]["hp"] - 10);
+	// Initial attack based on attacking character's Attack stats
+  var minDamage = characters[curChar]["attack"][0];
+  var maxDamage = characters[curChar]["attack"][1];
+  var realDamage = Math.floor(Math.random() * (maxDamage - minDamage + 1)) + minDamage;
+
+  // Find block percentage based on defending character's Defense stats
+  var minBlock = characters[charId]["defense"][0];
+  var maxBlock = characters[charId]["defense"][1];
+  var realBlock = Math.random() * (maxBlock - minBlock) + minBlock;
+
+  // Reduce initial damage by block percentage, then round down to nearest integer
+  var finalDamage = Math.floor(realDamage * realBlock);
+
+	characters[charId]["hp"] = (characters[charId]["hp"] - finalDamage);
+
 	var newHp = characters[charId]["hp"];
 	$('#' + charId + ' .hp').text(newHp);
+
 	var charName = characters[charId]["title"];
-	$('#action').append("<p>You attacked "+ charName + "!</p>");
+	$('#action').append("<p>You attacked "+ charName + "! " + charName + " lost " + finalDamage + " hp</p>");
 };
 
 var characters = {
@@ -42,4 +53,6 @@ var characters = {
 		"accuracy": 0.9
 	}
 };
+
+var curChar = "char1";
 
